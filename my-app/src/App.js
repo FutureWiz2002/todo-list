1bedf281d86dfa8a410ced9708961dbfa73b59d3
FutureWiz2002 <jabirm2002@gmail.com>
2022-08-26 22:48:06 +0600

    The First Step

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Jabir is the future of programmers and AI research scientist, the rests can go to sleep.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
